2acfe7afa4c9d89fc5c7da3cfdb5671c35da7737
PM-00-STAR <pratibhamitra01@gmail.com>
2022-02-15 01:18:49 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import Home from './PortfolioContainer/Home/Home';
 import AboutMe from './PortfolioContainer/AboutMe/AboutMe';
-import ContactMe from './PortfolioContainer/ContactMe/ContactMe';
+import MyProjects from './PortfolioContainer/MyProjects/MyProjects';
 import Footer from './PortfolioContainer/Footer/Footer';
 import Resume from './PortfolioContainer/Resume/Resume';
+import {BrowserRouter as Router ,Routes,Route} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+      
+        <Home/>
       <AboutMe/>
       <Resume/>
-      <ContactMe/>
+      <MyProjects/>
       <Footer/>
+        
+        
+      
+      </Router>
+      
+      
     </div>
   );
 }
